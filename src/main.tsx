7bd0dff56ec12938bb14f591077667a4d9573945
Open-Scar <gediyonadamu5070@gmail.com>
2026-05-28 00:49:48 +0300

    Fix Vite entry script and React startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "sonner";
-import ErrorBoundary from "@/components/ErrorBoundary";
 import AppRouter from "./router";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  throw new Error("Missing root element. index.html must include <div id=\"root\"></div>.");
+  throw new Error("Missing #root element in index.html");
 }
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <AppRouter />
-        <Toaster richColors position="top-right" />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </React.StrictMode>
 );
